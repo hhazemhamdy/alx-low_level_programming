f2e09fe9e46e6d183f2606ef42cade97fc718026
hhazemhamdy <hazemhamdy878@gmail.com>
2023-08-13 17:52:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  Entry point
+ * main - Entry point
  *
  * Description: A C program that prints with put function
  *
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	/* print a messege using puts */
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}	
+	/* print a message using puts */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
